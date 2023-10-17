3d879eb44d92a2799308492274782eeba1e7db52
Recrafter <renehurter0@gmail.com>
2023-10-17 19:42:46 +0200

    Adding mock and a test to nft-marketplace pallet

--- pallets/nft-marketplace/src/tests.rs
+++ pallets/nft-marketplace/src/tests.rs
@@ -2,26 +2,10 @@ use crate::{mock::*, Error, Event};
 use frame_support::{assert_noop, assert_ok};
 
 #[test]
-fn it_works_for_default_value() {
+fn list_object_works() {
 	new_test_ext().execute_with(|| {
-		// Go past genesis block so events get deposited
 		System::set_block_number(1);
-		// Dispatch a signed extrinsic.
-		assert_ok!(TemplateModule::do_something(RuntimeOrigin::signed(1), 42));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(TemplateModule::something(), Some(42));
-		// Assert that the correct event was deposited
-		System::assert_last_event(Event::SomethingStored { something: 42, who: 1 }.into());
-	});
-}
-
-#[test]
-fn correct_error_for_none_value() {
-	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-		assert_noop!(
-			TemplateModule::cause_error(RuntimeOrigin::signed(1)),
-			Error::<Test>::NoneValue
-		);
-	});
+		assert_ok!(NftMarketplace::list_object(RuntimeOrigin::signed([0; 32].into()), 1_000_000));
+		assert_eq!(NftMarketplace::listed_nfts().len(), 10);
+	})
 }
